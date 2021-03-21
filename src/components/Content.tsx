0f02cc0f2b459b5bdc5f2b679b6e211f4184c250
=Marcel Coelho <marcelfillipe@hotmail.com>
2021-03-21 12:55:01 -0300

    desafio -2

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
+import { useContext } from "react";
+import { ContextoMovies } from "../context/ContextoMovies";
+import { MovieCard } from "./MovieCard";
+
 export function Content() {
-  // Complete aqui
+  const { selectedGenre, movies } = useContext(ContextoMovies);
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre?.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies &&
+            movies.map((movie) => (
+              <MovieCard
+                key={movie.Title}
+                title={movie.Title}
+                poster={movie.Poster}
+                runtime={movie.Runtime}
+                rating={movie.Ratings[0].Value}
+              />
+            ))}
+        </div>
+      </main>
+    </div>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
+import { useContext, useEffect, useState } from "react";
+import { Button } from "./Button";
+
+import "../styles/sidebar.scss";
+import { ContextoMovies } from "../context/ContextoMovies";
+
 export function SideBar() {
-  // Complete aqui
+  const { genres, selectedGenreId, setSelectedGenreId } = useContext(
+    ContextoMovies
+  );
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres &&
+          genres.map((genre) => (
+            <Button
+              key={genre?.title}
+              title={genre?.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+      </div>
+    </nav>
+  );
 }
